1abf0d6c7d906c5b940fc6ddae7e79b005a0b83a
Balatestleaf <balamuralimay10@gmail.com>
2025-04-27 14:20:05 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args)
+	
+		{
+		float value=91.0f;
+				System.out.println(value);
+		
+		String developer="Google";
+				System.out.println(developer);
+			
+		boolean Beta=false;
+		        System.out.println(Beta);
+		
+		int release=2008;
+		        System.out.println(release);
+		       
+		char shortcutkey='c';
+		        System.out.println(shortcutkey);
+		
+		}
+	}	
+
+
+
